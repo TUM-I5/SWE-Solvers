5f539304888a21ce7c01c9d823e11a2250b64cc3
Vistemboir <marot.marc@googlemail.com>
2024-01-25 11:53:07 +0100

    Update FWaveSolver.hpp
    Fixed an error where the fwave solver with right dry state had the wrong velocity

--- Source/FWaveSolver.hpp
+++ Source/FWaveSolver.hpp
@@ -275,7 +275,7 @@ namespace Solvers {
         hRight_      = hLeft_;
         bRight_      = bLeft_;
         huRight_     = -huLeft_;
-        uLeft_       = -uRight_;
+        uRight_      = -uLeft_;
         wetDryState_ = WavePropagationSolver<T>::WetDryState::WetDryWall;
       } else { // Both cells wet
         uLeft_  = huLeft_ / hLeft_;
